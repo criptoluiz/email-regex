70c7a6a0c67e7e1ee66ce6172cfd0f133c9567de
luiz <luiz.pb@cryptolab.net>
2020-06-09 17:08:19 -0300

    timeout to reset css

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function validate() {
   let email = document.getElementById("email").value;
   let regx = /^([a-z0-9_-]+)+@(gmail|outlook|hotmail).([a-z]{2,8})(\.[a-z]{2,8})?$/;
@@ -8,6 +6,9 @@ function validate() {
     document.getElementById("result").innerHTML="e-mail válido";
     document.getElementById("result").style.visibility="visible"
     document.getElementById("result").style.color="white";
+    document.getElementById("email").style.borderColor="#59E659";
+    window.setTimeout(resetStyle, 2000);
+    //return false so the page doesn't update itself on keypress
     return false;
   }
   else
@@ -15,6 +16,15 @@ function validate() {
     document.getElementById("result").innerHTML="e-mail inválido";
     document.getElementById("result").style.visibility="visible"
     document.getElementById("result").style.color="pink";
+    document.getElementById("email").style.borderColor="#E65963";
+    window.setTimeout(resetStyle, 2000);
+    //return false so the page doesn't update itself on keypress
     return false;
   }
+}
+
+function resetStyle() {
+  document.getElementById("result").style.visibility="hidden"
+  document.getElementById("result").style.color="white";
+  document.getElementById("email").style.borderColor="white";
 }
